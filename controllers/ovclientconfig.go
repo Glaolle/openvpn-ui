b8bc19689c340bbc36700eb430dabe4274a3a14a
Glaolle <glaolle@gmail.com>
2026-05-27 07:47:19 +0300

    Change SIGHUP to SIGUSR1

--- controllers/ovclientconfig.go
+++ controllers/ovclientconfig.go
@@ -87,7 +87,7 @@ func (c *OVClientConfigController) Post() {
 	} else {
 		flash.Success("Post: Config has been updated")
 		client := mi.NewClient(state.GlobalCfg.MINetwork, state.GlobalCfg.MIAddress)
-		if err := client.Signal("SIGHUP"); err != nil {
+		if err := client.Signal("SIGUSR1"); err != nil {
 			flash.Warning("Config has been updated but OpenVPN server was NOT reloaded: " + err.Error())
 		}
 	}
